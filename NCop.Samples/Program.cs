10e1de83ed3a461d62c6a4250f9170546c7af353
Sagi <sagifogel@hotmail.com>
2015-11-12 21:41:18 +0200

    samples main program was restored

--- NCop.Samples/Program.cs
+++ NCop.Samples/Program.cs
@@ -1,52 +1,9 @@
-﻿using NCop.Samples.MultipleAspects.Events;
+﻿using NCop.Samples.MultipleAspecyts.Events;
 
 namespace NCop.Samples
 {
-    //[TransientComposite]
-    //[Mixins(typeof(CSharpDeveloperMixin))]
-    //public interface IDeveloper
-    //{
-    //    [MethodInterceptionAspect(typeof(InterceptionAspectImpl2))]
-    //    [MethodInterceptionAspect(typeof(AnotherInterceptionAspectImpl2))]
-    //    void Code();
-    //}
-
-    //public class CSharpDeveloperMixin : IDeveloper
-    //{
-    //    public void Code() {
-    //        Console.WriteLine("C# coding");
-    //    }
-    //}
-
-    //public class InterceptionAspectImpl2 : ActionInterceptionAspect
-    //{
-    //    public override void OnInvoke(ActionInterceptionArgs args) {
-    //        Console.WriteLine("OnInvoke of AnInterceptionAspect");
-    //        base.OnInvoke(args);
-    //    }
-    //}
-
-    //public class AnotherInterceptionAspectImpl2 : ActionInterceptionAspect
-    //{
-    //    public override void OnInvoke(ActionInterceptionArgs args) {
-    //        Console.WriteLine("OnInvoke of AnotherInterceptionAspect");
-    //        base.OnInvoke(args);
-    //    }
-    //}
-
     class Program
     {
-        //static void Main(string[] args) {
-        //    IDeveloper developer = null;
-        //    var container = new CompositeContainer(new CompositeRuntimeSettings {
-        //        Types = new[] { typeof(IDeveloper) }
-        //    });
-
-        //    container.Configure();
-        //    developer = container.Resolve<IDeveloper>();
-        //    developer.Code();
-        //}
-
         static void Main(string[] args) {
             CompositeRunner.Run();
         }
